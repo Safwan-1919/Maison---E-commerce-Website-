906d97f91c67959679a8e0c4b7352646981701bc
Safwan-1919 <4pa22cs072@pace.edu.in>
2026-07-05 22:42:49 +0530

    fix: remove catch-all route and not-found page that broke production

--- src/app/[...slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const SPAApp = dynamic(() => import("../page"), { ssr: false });
-
-export default function CatchAllPage() {
-  return <SPAApp />;
-}
